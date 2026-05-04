d90a72c6240c57a3eab1bd547208696666d6791d
aarondchalu <aarondchalu@gmail.com>
2026-05-04 23:49:39 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -219,7 +219,7 @@ export default function Home() {
         
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-10">
           <div className="text-center mb-16">
-            <h2 className="text-3xl md:text-4xl font-bold mb-4">Alur Pendaftaran PPDB</h2>
+            <h2 className="text-3xl md:text-4xl font-bold mb-4">Alur Pendaftaran PMBM</h2>
             <p className="text-slate-400 max-w-2xl mx-auto text-lg">
               Ikuti langkah-langkah mudah berikut untuk mendaftarkan putra/putri Anda di MTs. DDI Al-Kautsar.
             </p>
